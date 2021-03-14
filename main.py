8a30d053fe28d7c0feac524389e7a51d4b4f2c86
Dogan G <dogan.guendogdu@tutanota.com>
2021-03-14 08:24:30 +0100

    added main function

--- main.py
+++ main.py
@@ -1,73 +1,84 @@
 import datetime 
+import os
+import sys
 import userInput    as ui
 import karteikarten as kk
 import database     as db
 
-# Begruessung   
-print()
-print("Wilkommen zum Lernprogramm")
-print("Bitte beachte die Gross,- und Kleinschreibung")
-print("Bitte waehlen Sie ihre Csv-Datei aus")
 
-#Lese die Box 1 durch den User ein
-box1    = ui.inputFile()
+def main():
+     
+    # Begruessung   
+    print("\nWilkommen zum Lernprogramm")
+    print("Bitte beachte die Gross,- und Kleinschreibung")
+    print("Bitte waehlen Sie ihre Csv-Datei aus")
+    
+    #Lese die Box 1 durch den User ein und checke auf Fehler
+    box1    = ui.inputFile()
+    
+    # Checke das Format der entgegengenommenen Datei
+    kk.Karteikarten.checkFormatBox1(box1)
+    
+    # print("\nWie viele Minuten wollen Sie lernen?")
+    
+     # Aktuelle Zeit im utc Format
+    # startTime = datetime.datetime.utcnow()
+    
+    # Endzeit besteht aus der aktuellen Zeit plus der angegebenen
+    # Minuten, die man lernen möchte
+    # endTime = startTime + datetime.timedelta(minutes=ui.getStudyTime())
+    
+    
+    # if datetime.datetime.utcnow() > endTime:
+    #     pass 
+    #     print("Zeitlimit ist abgelaufen!") 
+    
+    
+    # # Karteiprogramm fuer Box 1
+    print("\nBox 1")
+    k1 = kk.Karteikarten(box1, box1, "files/Box_2.csv")
+    k1.checkBox1()
+    
+    # # Karteiprogramm fuer Box 2
+    print("\nBox 2")
+    k2 = kk.Karteikarten(box1, "files/Box_2.csv", "files/Box_3.csv")
+    k2.checkOtherBoxes() 
+    
+    # # Karteiprogramm fuer Box 3
+    print("\nBox 3")
+    k3 = kk.Karteikarten(box1, "files/Box_3.csv", "files/Box_4.csv")
+    k3.checkOtherBoxes()
 
+    # Karteiprogramm fuer Box 4
+    print("\nBox 4")
+    k4 = kk.Karteikarten(box1, "files/Box_4.csv", "files/Box_5.csv")
+    k4.checkOtherBoxes()
 
-# Checke das Format der entgegengenommenen Datei
-kk.Karteikarten.checkFormatBox1(box1)
-
-print("\nWie viele Minuten wollen Sie lernen?")
-
- # Aktuelle Zeit im utc Format
-startTime = datetime.datetime.utcnow()
-
-# Endzeit besteht aus der aktuellen Zeit plus der angegebenen
-# Minuten, die man lernen möchte
-endTime = startTime + datetime.timedelta(minutes=ui.getStudyTime())
-
-
-# if datetime.datetime.utcnow() > endTime:
-#     pass 
-#     print("Zeitlimit ist abgelaufen!") 
-
-
-# # # Karteiprogramm fuer Box 1
-# print("\nBox 1")
-# k1 = kk.Karteikarten(box1, box1, "files/Box_2.csv")
-# k1.checkBox1()
-
-# # # Karteiprogramm fuer Box 2
-# print("\nBox 2")
-# k2 = kk.Karteikarten(box1, "files/Box_2.csv", "files/Box_3.csv")
-# k2.checkOtherBoxes() 
-
-# # # Karteiprogramm fuer Box 3
-# print("\nBox 3")
-# k3 = kk.Karteikarten(box1, "files/Box_3.csv", "files/Box_4.csv")
-# k3.checkOtherBoxes()
-
-# # Karteiprogramm fuer Box 4
-# print("\nBox 4")
-# k4 = kk.Karteikarten(box1, "files/Box_4.csv", "files/Box_5.csv")
-# k4.checkOtherBoxes()
-
-# # # Karteiprogramm fuer Box 5
-# print("\nBox 5")
-# k5 = kk.Karteikarten(box1, "files/Box_5.csv", None)
-# k5.checkBox5()
-
-
-#                 #Statistik
-# #----------------------------------------------#
-# r = kk.Karteikarten(None,None,None)
-# # Gebe Statistik ueber gestelle Fragen aus
-# r.getStatisticOfQuestion()
-# # Gebe aus, wie viele Fragen noch in den Boxen
-# r.getNumberOfQuestions(box1)
-# # schaue, ob alle Fragen beantwortet wurden
-# r.getFinalAnswer(box1)
+    # # Karteiprogramm fuer Box 5
+    print("\nBox 5")
+    k5 = kk.Karteikarten(box1, "files/Box_5.csv", None)
+    k5.checkBox5()
 
 
+                    #Statistik
+    #----------------------------------------------#
+    r = kk.Karteikarten(None,None,None)
+    # Gebe Statistik ueber gestelle Fragen aus
+    r.getStatisticOfQuestion()
+    # Gebe aus, wie viele Fragen noch in den Boxen
+    r.getNumberOfQuestions(box1)
+    # schaue, ob alle Fragen beantwortet wurden
+    r.getFinalAnswer(box1)
 
 
-# db.createDatabase()
+    # db.createDatabase()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nProgramm wurde beendet.")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
